94d2badd76322f8b4d2f682781aeed7fba64f685
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-25 20:17:00 -0500

    add license

--- contracts/mocks/MockTimelock.sol
+++ contracts/mocks/MockTimelock.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
-
+// SPDX-License-Identifier: MIT AND AGPL-3.0-or-later
 pragma solidity =0.8.9;
 
 import "../governance/Timelock.sol";
--- contracts/staking-rewards/Owned.sol
+++ contracts/staking-rewards/Owned.sol
@@ -1,6 +1,6 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT AND AGPL-3.0-or-later
 pragma solidity 0.8.9;
-  
+
 contract Owned {
     address public owner;
     address public nominatedOwner;
